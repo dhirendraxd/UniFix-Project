30405241640f493b304d9cf8203abc019c5ea527
Dhirendra Singh Dhami <dhirendraxd@gmail.com>
2025-02-04 21:45:15 +0545

    Remove commented export statement from Accordion component

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -54,3 +54,7 @@ const AccordionContent = React.forwardRef<
 AccordionContent.displayName = AccordionPrimitive.Content.displayName
 
 export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
+
+// export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
+
+
